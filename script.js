ed9f2a0bd67d934e4758921c01eb696e901b4e0f
faizadnan <faizadnan1h@gmail.com>
2022-10-23 12:29:44 +0530

    counter added

--- script.js
+++ script.js
@@ -34,8 +34,9 @@ let i = 0;
 let corrctAns = 0;
 let submitTest = document.getElementById("submit_test");
 let next_question = document.getElementById("next_question");
-let music = new Audio("./music1.wav")      
-let winner = new Audio("./winner1.mp3")      
+let music = new Audio("./music1.wav")
+let winner = new Audio("./winner1.mp3");
+let count = 31;
 arr.map(items => {
     document.getElementById("question").innerText = arr[i].question;
     document.querySelector("#Correct_ans").value = arr[i].ans1
@@ -45,74 +46,104 @@ arr.map(items => {
 
 })
 
-document.getElementById("next_question").addEventListener("click", function () {
+function moveToNextQues () {
     i = i + 1;
-    console.log("clicked");
+
+    //using this if conditon so that once the last question timer ends, the counter hides...and page redirect to score page
+        if(i==arr.length){
+        document.getElementById("counter").style.display = "none";
+        submitQuiz()
+    }  
+
     arr.map(items => {
         Array.from(document.querySelectorAll("input")).forEach(items => items.removeAttribute("disabled"))
         document.getElementById("question").innerText = arr[i].question;
         document.querySelector("#Correct_ans").value = arr[i].ans1
         document.querySelector(".Wrong_ans1").value = arr[i].ans2
         document.querySelector(".Wrong_ans2").value = arr[i].ans3
-        document.querySelector(".Wrong_ans3").value = arr[i].ans4            
+        document.querySelector(".Wrong_ans3").value = arr[i].ans4
         document.querySelectorAll("input").forEach(item => {
             item.style.color = "white";
             item.style.backgroundColor = "darkslategrey"
-        })
-    })
-    console.log(i)
-    if (i == (arr.length-1)) {
-        submitTest.style.display = "block"
-        next_question.style.display = "none"
-    }
+        });
+        count=31;console.log(i)
+        if (i == (arr.length - 1)) {
+            submitTest.style.display = "block";
+            
+            next_question.style.display = "none"
+        }
+       
+    })}
     
-})
 
-document.querySelectorAll(".Wrong").forEach(items => {   
-    items.addEventListener("click", function (event) {        
+
+document.getElementById("next_question").addEventListener("click", moveToNextQues)
+
+document.querySelectorAll(".Wrong").forEach(items => {
+    items.addEventListener("click", function (event) {
         items.style.color = "black";
         items.style.backgroundColor = "red";
-      
+
 
     })
 })
 
-document.querySelector("#Correct_ans").addEventListener("click", function () {   
+document.querySelector("#Correct_ans").addEventListener("click", function () {
     this.style.backgroundColor = "green"
-    this.style.color = "black";  
-    corrctAns =corrctAns+1
+    this.style.color = "black";
+    corrctAns = corrctAns + 1
     console.log(`coreadns= ${corrctAns}`)
 
 })
 
-submitTest.addEventListener("click", function(){
-    document.querySelector(".container").style.display="none"
+function submitQuiz() {
+    document.querySelector(".container").style.display = "none"
     document.querySelector(".result_div").classList.add("showScore")
-    document.querySelector(".buttons").style.display="none"
+    document.querySelector(".buttons").style.display = "none"
     console.log(`coreadnsfghjk= ${corrctAns}`);
-    let result = (corrctAns>=(arr.length/2+1)) ? `Great! your score is ${corrctAns}/${arr.length} ` : `Oops! your score is ${corrctAns}/${arr.length}`;            
+    let result = (corrctAns >= (arr.length / 2 + 1)) ? `Great! your score is ${corrctAns}/${arr.length} ` : `Oops! your score is ${corrctAns}/${arr.length}`;
     document.getElementById("score").insertAdjacentText("afterbegin", result);
     winner.play()
-})
+}
+submitTest.addEventListener("click",submitQuiz )
 
 
 document.querySelectorAll("input").forEach(input => {
-    input.addEventListener("click", function(){
-        input.classList.add("efg");       
-        Array.from(document.querySelectorAll("input")).filter(item => !item.classList.contains("efg")).forEach(items => items.setAttribute("disabled", "disabled"))  ;
-        music.play()     
+    input.addEventListener("click", function () {
+        input.classList.add("efg");
+        Array.from(document.querySelectorAll("input")).filter(item => !item.classList.contains("efg")).forEach(items => items.setAttribute("disabled", "disabled"));
+        music.play()
     })
 })
 
-document.getElementById("Start_quiz").addEventListener("click", function(){
+document.getElementById("Start_quiz").addEventListener("click", function () {
     this.style.display = "none";
-    document.getElementById("loading").innerText= "Loading..."
-    setTimeout(function(){
-        document.getElementById("loading").style.display="none"
-        document.querySelector(".container").style.display="flex";
-    document.querySelector("#next_question").style.display="block";
-   
-    },2000)
-    
+    document.getElementById("loading").innerText = "Loading..."
+    setTimeout(function () {
+        document.getElementById("loading").style.display = "none"
+        document.querySelector(".container").style.display = "flex";
+        document.querySelector("#next_question").style.display = "block";
+         //seting counter ON:-
+         document.getElementById("counter").style.display = "inline-block";  
+       
+         setInterval(function () {
+            console.log("set interval is woking")
+            if (count > 0) {
+                count = count - 1;
+            } else {
+                count = 30
+            }
+            if(count==0){
+                moveToNextQues()
+            }
+            document.getElementById("counter").innerText = count;
+        }, 1000);
+            
+       
+    }, 2000)
+
 })
- 
+
+
+
+
